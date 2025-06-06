da181da9a2121d3a668ba6de94eeaa4e11072037
Shubham Nimje <shubhamsnimje@gmail.com>
2025-06-06 13:10:31 +0530

    day1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InterpolationComponent } from './pages/day1/interpolation/interpolation.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InterpolationComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'ang19-tutorial';
+  title = 'Shubham S. Nimje';
 }
---
+++ src/app/pages/day1/interpolation/interpolation.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interpolation',
+  imports: [],
+  templateUrl: './interpolation.component.html',
+  styleUrl: './interpolation.component.css',
+})
+export class InterpolationComponent {
+  x = 4;
+  y = 5;
+  user1 = 'Shubham';
+  user2 = 'Sanket';
+}
