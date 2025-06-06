935f5b657cfbb092eac5b690424ec69ff864fe2b
Shubham Nimje <shubhamsnimje@gmail.com>
2025-06-06 22:31:06 +0530

    day19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { InterpolationComponent } from './pages/day1/interpolation/interpolation.component';
-import { AngularcliComponent } from './pages/day2/angularcli/angularcli.component';
-import { ComponentsComponent } from './pages/day3/components/components.component';
-import { BtnandfunctionComponent } from './pages/day4/btnandfunction/btnandfunction.component';
-import { DatatypesComponent } from './pages/day5/datatypes/datatypes.component';
-import { CounterappComponent } from './pages/day6/counterapp/counterapp.component';
-import { EventsComponent } from './pages/day7/events/events.component';
-import { InputfieldvalueComponent } from './pages/day8/inputfieldvalue/inputfieldvalue.component';
-import { StyleoptionComponent } from './pages/day9/styleoption/styleoption.component';
-import { IfelseComponent } from './pages/day10/ifelse/ifelse.component';
-import { ElseifComponent } from './pages/day11/elseif/elseif.component';
-import { SwitchcaseComponent } from './pages/day12/switchcase/switchcase.component';
-import { ForloopComponent } from './pages/day13/forloop/forloop.component';
-import { SignalsComponent } from './pages/day14/signals/signals.component';
-import { DatatypewithsignalsComponent } from './pages/day15/datatypewithsignals/datatypewithsignals.component';
-import { ComputedsignalsComponent } from './pages/day16/computedsignals/computedsignals.component';
-import { EffectinngComponent } from './pages/day17/effectinng/effectinng.component';
-import { LoopcontextualComponent } from './pages/day18/loopcontextual/loopcontextual.component';
+import { InterpolationComponent } from './components/day1/interpolation/interpolation.component';
+import { AngularcliComponent } from './components/day2/angularcli/angularcli.component';
+import { ComponentsComponent } from './components/day3/components/components.component';
+import { BtnandfunctionComponent } from './components/day4/btnandfunction/btnandfunction.component';
+import { DatatypesComponent } from './components/day5/datatypes/datatypes.component';
+import { CounterappComponent } from './components/day6/counterapp/counterapp.component';
+import { EventsComponent } from './components/day7/events/events.component';
+import { InputfieldvalueComponent } from './components/day8/inputfieldvalue/inputfieldvalue.component';
+import { StyleoptionComponent } from './components/day9/styleoption/styleoption.component';
+import { IfelseComponent } from './components/day10/ifelse/ifelse.component';
+import { ElseifComponent } from './components/day11/elseif/elseif.component';
+import { SwitchcaseComponent } from './components/day12/switchcase/switchcase.component';
+import { ForloopComponent } from './components/day13/forloop/forloop.component';
+import { SignalsComponent } from './components/day14/signals/signals.component';
+import { DatatypewithsignalsComponent } from './components/day15/datatypewithsignals/datatypewithsignals.component';
+import { ComputedsignalsComponent } from './components/day16/computedsignals/computedsignals.component';
+import { EffectinngComponent } from './components/day17/effectinng/effectinng.component';
+import { LoopcontextualComponent } from './components/day18/loopcontextual/loopcontextual.component';
+import { TwowaybindingComponent } from './components/day19/twowaybinding/twowaybinding.component';
 
 @Component({
   selector: 'app-root',
@@ -41,6 +42,7 @@ import { LoopcontextualComponent } from './pages/day18/loopcontextual/loopcontex
     ComputedsignalsComponent,
     EffectinngComponent,
     LoopcontextualComponent,
+    TwowaybindingComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
