ff7000d897a57e9dd0e8863ab3d149032c03ecd6
Shubham Nimje <shubhamsnimje@gmail.com>
2025-06-06 14:15:18 +0530

    day3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InterpolationComponent } from './pages/day1/interpolation/interpolation.component';
 import { AngularcliComponent } from './pages/day2/angularcli/angularcli.component';
+import { ComponentsComponent } from './pages/day3/components/components.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, InterpolationComponent, AngularcliComponent],
+  imports: [
+    RouterOutlet,
+    InterpolationComponent,
+    AngularcliComponent,
+    ComponentsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
